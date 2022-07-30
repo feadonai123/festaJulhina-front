fbc79df94d02a35c9211561f61d3de6d0d633498
feadonai123 <felipe.2002@alunos.utfpr.edu.br>
2022-07-30 20:02:11 -0300

    Persistencia itens carrinho ao recarregar página

--- hooks/carrinho.js
+++ hooks/carrinho.js
@@ -1,10 +1,25 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import LocalStorage from '../utils/localStorage';
 
 const CarrinhoContext = createContext();
 
 const CarrinhoProvider = ({ children }) => {
 
   const [ carrinho, setCarrinho ] = useState([])
+
+  useEffect(()=>{
+    try{
+      const 
+        carrinho = LocalStorage.get("carrinho"),
+        carrinhoFormat = JSON.parse(carrinho)
+      if(!carrinhoFormat || carrinhoFormat.length == 0) return
+      setCarrinho(carrinhoFormat)
+    }catch(e){}
+  }, [])
+
+  useEffect(()=>{
+    LocalStorage.set("carrinho", JSON.stringify(carrinho))
+  }, [ carrinho ])
 
   const addCarrinho = (data)=>{
     const tempCarrinho = [...carrinho]
